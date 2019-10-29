cf929a09af8ea12430bd96bcccebe1d48587c7ae
James Chetwood <james@jameschetwood.co.uk>
2019-10-29 09:39:27 +0000

    Tidy

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,10 +1,7 @@
 export const add = () => async dispatch => {
   const res = await fetch("https://swapi.co/api/people/");
-  // console.log({ res });
   const res2 = await res.json();
-  // console.log({ res2 });
   const people = res2.results;
-  // console.log({ people });
 
   return dispatch({
     type: "ADD",
--- src/store/add.spec.js
+++ src/store/add.spec.js
@@ -1,19 +1,18 @@
 import { add } from "./actions";
 
 test("add2", async () => {
-  global.fetch = jest.fn(() => {
-    return {
-      json: jest.fn(() => {
-        return {
-          results: ["bobby"]
-        };
-      })
-    };
-  });
+  const mockFetchJson = jest.fn(() => ({ results: ["tom", "jim"] }));
+  global.fetch = jest.fn(() => ({ json: mockFetchJson }));
 
   const dispatch = jest.fn();
 
   await add()(dispatch);
 
-  console.log(dispatch.mock.calls);
+  expect(dispatch.mock.calls.length).toBe(1);
+  expect(dispatch.mock.calls[0]).toEqual([
+    {
+      type: "ADD",
+      people: ["tom", "jim"]
+    }
+  ]);
 });
