67cf5e10bab2b003575404cd74c2a094e733fce2
James Chetwood <james@jameschetwood.co.uk>
2019-10-29 09:44:20 +0000

    clearAllMocks

--- src/store/add.spec.js
+++ src/store/add.spec.js
@@ -1,18 +1,26 @@
 import { add } from "./actions";
 
-test("add2", async () => {
+describe("add", () => {
+  afterEach(jest.clearAllMocks);
+
   const mockFetchJson = jest.fn(() => ({ results: ["tom", "jim"] }));
   global.fetch = jest.fn(() => ({ json: mockFetchJson }));
 
   const dispatch = jest.fn();
 
-  await add()(dispatch);
+  test("first", async () => {
+    await add()(dispatch);
+    expect(dispatch.mock.calls.length).toBe(1);
+    expect(dispatch.mock.calls[0]).toEqual([
+      {
+        type: "ADD",
+        people: ["tom", "jim"]
+      }
+    ]);
+  });
 
-  expect(dispatch.mock.calls.length).toBe(1);
-  expect(dispatch.mock.calls[0]).toEqual([
-    {
-      type: "ADD",
-      people: ["tom", "jim"]
-    }
-  ]);
+  test("second", async () => {
+    // Will only be [] as clearAllMocks has been run
+    expect(dispatch.mock.calls.length).toBe(0);
+  });
 });
